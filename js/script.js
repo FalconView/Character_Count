3e3808c44ab978c77f3c3586081504a26a8b6ba6
Feroz Zargar <Ferozzargar@hotmail.com>
2022-09-22 00:16:10 -0400

    style.css was added and index.html was styled

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let textbox = document.getElementById("textbox");
+let count = document.getElementById("count");
+function updateCount() {
+  count.innerHTML = textbox.value.length;
+}
+textbox.oninput = updateCount;
